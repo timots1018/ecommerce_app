a68d2d31f4d5c060110dca4fd24bc42425ec0fdc
timots1018 <timothyjueangway@gmail.com>
2025-09-18 14:44:08 +0800

    add initial files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
